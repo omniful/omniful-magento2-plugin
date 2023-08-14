2eef3c86ab8ecbe5002468f407059ffb4c683ba3
Jaimin Sutariya <jsutariya@gmail.com>
2023-08-14 19:47:19 +0530

    Made tracking link and pdf optional

--- src/Core/Api/Sales/ShipmentInterface.php
+++ src/Core/Api/Sales/ShipmentInterface.php
@@ -11,8 +11,8 @@ interface ShipmentInterface
      * Create Order Shipment
      *
      * @param int $id The ID of the order to create a shipment for.
-     * @param string $tracking_link The tracking link for the shipment.
      * @param string $tracking_number The tracking number for the shipment.
+     * @param string $tracking_link The tracking link for the shipment.
      * @param string $shipping_label_pdf The shipping label PDF for the shipment.
      * @param string $carrier_title The carrier title for the shipment (optional).
      * @param bool $override_exist_data Whether to override existing shipment data (optional, default: false).
@@ -20,9 +20,9 @@ interface ShipmentInterface
      */
     public function processShipment(
         int $id,
-        string $tracking_link,
         string $tracking_number,
-        string $shipping_label_pdf,
+        string $tracking_link = null,
+        string $shipping_label_pdf = null,
         string $carrier_title = null,
         bool $override_exist_data = false
     );
