e8c798489629dea23219d3d46a6bfa926a262298
Jaimin <jsutariya@gmail.com>
2023-09-01 12:02:53 +0530

    Order after event not working from rest issue solved!

--- src/Core/Observer/Sales/OrderPlaceAfter.php
+++ src/Core/Observer/Sales/OrderPlaceAfter.php
@@ -2,16 +2,33 @@
 
 namespace Omniful\Core\Observer\Sales;
 
+use Exception;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
-use Omniful\Core\Model\Adapter;
-use Omniful\Core\Logger\Logger;
-use Omniful\Core\Model\Sales\Order as OrderManagement;
+use Magento\Framework\Stdlib\DateTime\DateTime;
+use Magento\Sales\Model\Order;
 use Magento\Store\Model\StoreManagerInterface;
+use Omniful\Core\Logger\Logger;
+use Omniful\Core\Model\Adapter;
+use Omniful\Core\Model\Sales\Order as OrderManagement;
+use Omniful\Core\Model\Sales\Status;
 
 class OrderPlaceAfter implements ObserverInterface
 {
     public const ORDER_CREATED_EVENT_NAME = "order.created";
+    public const ORDER_STATUS_UPDATED_EVENT_NAME = "order.status.updated";
+
+    public const ALLOWED_STATUSES = [
+        Order::STATE_NEW,
+        Order::STATE_PENDING_PAYMENT,
+        Order::STATE_PROCESSING,
+        Status::STATUS_READY_TO_SHIP,
+        Status::STATUS_SHIPPED,
+        Status::STATUS_REFUNDED,
+        Status::STATUS_PACKED,
+        Status::STATUS_DELIVERED,
+        Order::STATE_COMPLETE,
+    ];
 
     /**
      * @var Logger
@@ -25,23 +42,28 @@ class OrderPlaceAfter implements ObserverInterface
      * @var OrderManagement
      */
     protected $orderManagement;
-
     /**
      * @var StoreManagerInterface
      */
     protected $storeManager;
+    /**
+     * @var DateTime
+     */
+    private $date;
 
     /**
      * OrderPlaceAfter constructor.
      *
      * @param Logger $logger
      * @param Adapter $adapter
+     * @param DateTime $date
      * @param OrderManagement $orderManagement
      * @param StoreManagerInterface $storeManager
      */
     public function __construct(
         Logger $logger,
         Adapter $adapter,
+        DateTime $date,
         OrderManagement $orderManagement,
         StoreManagerInterface $storeManager
     ) {
@@ -49,6 +71,7 @@ class OrderPlaceAfter implements ObserverInterface
         $this->adapter = $adapter;
         $this->storeManager = $storeManager;
         $this->orderManagement = $orderManagement;
+        $this->date = $date;
     }
 
     /**
@@ -63,34 +86,58 @@ class OrderPlaceAfter implements ObserverInterface
         $store = $order->getStore();
 
         try {
-            $eventName = self::ORDER_CREATED_EVENT_NAME;
-            $storeData = $this->storeManager->getGroup($store->getGroupId());
-
-            $headers = [
-                "x-website-code" => $order
-                    ->getStore()
-                    ->getWebsite()
-                    ->getCode(),
-                "x-store-code" => $storeData->getCode(),
-                "x-store-view-code" => $order->getStore()->getCode(),
-            ];
-
-            // Connect to the adapter
-            $this->adapter->connect();
-
-            // Publish the event if the event name is not empty
-            if ($eventName !== "") {
-                $payload = $this->orderManagement->getOrderData($order);
-                // Log the successful publication of the order event
-                $this->logger->info(__("Order event published successfully"));
-                return $this->adapter->publishMessage(
-                    $eventName,
-                    $payload,
-                    $headers
-                );
+            $currentDateTime = strtotime($this->date->gmtDate());
+            $orderDateTime = strtotime($order->getCreatedAt());
+            $differenceInSeconds = $currentDateTime - $orderDateTime;
+            if ($differenceInSeconds < '5' && $order->getOrigData("status") === null &&
+                $order->getStatus() !== Order::STATE_CANCELED) {
+                $eventName = self::ORDER_CREATED_EVENT_NAME;
+                return $this->getOrderDataByEventName($store, $order, $eventName);
+            } elseif ($order->getStatus() !== Order::STATE_CANCELED &&
+                $order->getStatus() !== $order->getOrigData("status") &&
+                in_array($order->getStatus(), self::ALLOWED_STATUSES)) {
+                $eventName = self::ORDER_STATUS_UPDATED_EVENT_NAME;
+                return $this->getOrderDataByEventName($store, $order, $eventName);
             }
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             $this->logger->error(__($e->getMessage()));
+        }
+    }
+
+    /**
+     * Get Order Data By Event Name
+     *
+     * @param mixed $store
+     * @param mixed $order
+     * @param mixed $eventName
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function getOrderDataByEventName($store, $order, $eventName)
+    {
+        $storeData = $this->storeManager->getGroup($store->getGroupId());
+
+        $headers = [
+            "x-website-code" => $order
+                ->getStore()
+                ->getWebsite()
+                ->getCode(),
+            "x-store-code" => $storeData->getCode(),
+            "x-store-view-code" => $order->getStore()->getCode(),
+        ];
+
+        // Connect to the adapter
+        $this->adapter->connect();
+
+        // Publish the event if the event name is not empty
+        if ($eventName !== "") {
+            $payload = $this->orderManagement->getOrderData($order);
+            // Log the successful publication of the order event
+            $this->logger->info(__("Order event published successfully"));
+            return $this->adapter->publishMessage(
+                $eventName,
+                $payload,
+                $headers
+            );
         }
     }
 }
